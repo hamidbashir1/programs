9be045547b87e84bb4c5c86d1b735796c18283c7
hamidbashir1 <pir.hamid.mujtaba@gmail.com>
2017-11-28 08:25:16 +0530

    initial commit

---
+++ detEmp.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct emp
+{
+    int empno ;
+    char name[10] ;
+    int basicPay, allowances, deduction, netPay ;
+} e[10] ;
+void main()
+{
+    int i, n ;
+    printf("Enter the number of employees : ") ;
+    scanf("%d", &n) ;
+    for(i = 0 ; i < n ; i++)
+    {
+        printf("\nEnter the employee number : ") ;
+        scanf("%d", &e[i].empno) ;
+        printf("\nEnter the name : ") ;
+        scanf("%s", e[i].name) ;
+        printf("\nEnter the basic pay, allowances & deductions : ") ;
+        scanf("%d %d %d", &e[i].basicPay, &e[i].allowances, &e[i].deduction) ;
+        e[i].netPay = e[i].basicPay + e[i].allowances - e[i].deduction ;
+    }
+    printf("\n Emp. No.\tName   \t BasicPay \t allowances \t Deduction \t NetPay \n\n") ;
+    for(i = 0 ; i < n ; i++)
+    {
+        printf("\n\n  %d\t\t%s\t %d \t %d \t \t  %d \t \t%d \n\n", e[i].empno,
+        e[i].name, e[i].basicPay, e[i].allowances, e[i].deduction, e[i].netPay) ;
+    }
+
+}
